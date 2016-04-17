fcc6231b1c9b83f85a9bbfbdbb2eab56215dff65
CAPSLOCKUSER <m.miklos05@gmail.com>
2016-04-17 18:18:59 +0200

    added eslint

--- js/common/App/App.js
+++ js/common/App/App.js
@@ -1,22 +1,14 @@
 import React, {
-  Component,
   View,
   Text,
 } from 'react-native';
 
 import s from './App.style';
 
-class App extends Component {
-
-  static propTypes = {};
-
-  render() {
-    return (
-      <View style={s.root}>
-        <Text>Test</Text>
-      </View>
-    );
-  }
-}
+const App = () => (
+  <View style={s.root}>
+    <Text>Test</Text>
+  </View>
+);
 
 export default App;
